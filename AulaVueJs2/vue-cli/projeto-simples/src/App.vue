1b95aa7afc049328a31383150cfa04bb7af54f73
DiogoFeld <43175839+DiogoFeld@users.noreply.github.com>
2022-07-21 16:25:05 -0300

    Add files via upload

---
+++ AulaVueJs2/vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">{{ titulo }}
+  <button @click="titulo += '!!'">Alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      titulo: "testeDataUsandoVue.js",
+    };
+  },
+};
+</script>
+ 
+<style>
+#app
+{
+  background-color: chocolate;
+  color:wheat;
+}
+</style>
