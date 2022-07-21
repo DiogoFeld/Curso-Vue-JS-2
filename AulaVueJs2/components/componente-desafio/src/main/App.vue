bcbbacdfdc373b229386fdc82febd0e924214c87
DiogoFeld <43175839+DiogoFeld@users.noreply.github.com>
2022-07-21 16:34:50 -0300

    Add files via upload

---
+++ AulaVueJs2/components/componente-desafio/src/main/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-navigation-app />
+    <v-bar-app />
+    <v-corousel-app />
+    <v-footer-app />
+  </v-app>
+</template>
+
+<script>
+import navigation_app from "@/components/template/v-navigation.vue";
+import corousel_app from "@/components/widgtes/corousel.vue";
+import bar_app from "@/components/template/app-bar.vue";
+import footer_app from "@/components/template/footer.vue";
+
+export default {
+  components: {
+    "v-navigation-app": navigation_app,"v-bar-app": bar_app,
+    "v-corousel-app": corousel_app,"v-footer-app": footer_app,
+  },
+};
+</script>
